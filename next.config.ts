eb28cf7a5d039341853677d47a0213803d557f93
lisizejnullahu <lisi.zejnullahu@hotmail.com>
2025-02-15 01:03:47 -0500

    eslint ignore for deployment

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  eslint: {
+    ignoreDuringBuilds: true, // This will disable ESLint in Vercel deployment
+  },
 }
 
 export default nextConfig
